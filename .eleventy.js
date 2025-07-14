86c6dd41a081878fd94df71288bc3c58d10a496a
anaisf-hub <anais.fotsing@columbiaspectator.com>
2025-07-14 14:02:50 -0400

    Build to root for GitHub Pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,11 @@
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("src/style.css"); 
+    eleventyConfig.addPassthroughCopy("src/images");
+
+    eleventyConfig.addCollection("projects", function(collectionApi) {
+    return collectionApi.getFilteredByTag("project");
+  });
 
   return {
     dir: {
